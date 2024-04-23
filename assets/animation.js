76b03d1077de31e0cefdf328f9126aa1a384239a
Minkyung Kim <kimm037@newschool.edu>
2024-04-22 21:51:06 -0400

    mobile

--- assets/animation.js
+++ assets/animation.js
@@ -1,27 +1,27 @@
  // JavaScript for redirecting to the landing page after animation
- document.addEventListener('DOMContentLoaded', function() {
-    var animationsCount = 0;
+//  document.addEventListener('DOMContentLoaded', function() {
+//     var animationsCount = 0;
 
-    // Function to redirect to the landing page
-    function redirectToLandingPage() {
-        window.location.href = 'landingpage.html';
-    }
+//     // Function to redirect to the landing page
+//     function redirectToLandingPage() {
+//         window.location.href = 'landingpage.html';
+//     }
 
-    // Function to check if all animations are finished
-    function checkAllAnimationsFinished() {
-        animationsCount++;
-        if (animationsCount === 3) { // Assuming there are 3 animations
-            redirectToLandingPage();
-        }
-    }
+//     // Function to check if all animations are finished
+//     function checkAllAnimationsFinished() {
+//         animationsCount++;
+//         if (animationsCount === 3) { // Assuming there are 3 animations
+//             redirectToLandingPage();
+//         }
+//     }
 
-    // Listen for animation end events on fade, size, and moveUp elements
-    document.querySelector('.fade').addEventListener('animationend', checkAllAnimationsFinished);
-    document.querySelector('.size').addEventListener('animationend', checkAllAnimationsFinished);
-    document.querySelector('.intro').addEventListener('animationend', function(event) {
-        // Check if the animation that ended is the moveUp animation
-        if (event.animationName === 'moveUp') {
-            checkAllAnimationsFinished(); // Check for completion after moveUp animation
-        }
-    });
-});
+//     // Listen for animation end events on fade, size, and moveUp elements
+//     document.querySelector('.fade').addEventListener('animationend', checkAllAnimationsFinished);
+//     document.querySelector('.size').addEventListener('animationend', checkAllAnimationsFinished);
+//     document.querySelector('.intro').addEventListener('animationend', function(event) {
+//         // Check if the animation that ended is the moveUp animation
+//         if (event.animationName === 'moveUp') {
+//             checkAllAnimationsFinished(); // Check for completion after moveUp animation
+//         }
+//     });
+// });
